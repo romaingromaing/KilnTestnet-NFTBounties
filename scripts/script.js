c21e5c26a89e021eaa4b12654d3a08aaa2f3a750
Matteo Casonato <matteocasonato@protonmail.com>
2022-05-26 11:27:46 +0200

    Edit script

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,51 @@
 const fs = require('fs');
 
-async function deployContract() {
+function millisToMinutesAndSeconds(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
+async function deployContract(i) {
   const provider = ethers.providers.getDefaultProvider(process.env.URL)
   const privateKey = process.env.PRIVATE_KEY
   const wallet = new ethers.Wallet(privateKey, provider)
   const metadata = JSON.parse(fs.readFileSync('artifacts/contracts/Counter.sol/Counter.json').toString())
   const factory = new ethers.ContractFactory(metadata.abi, metadata.bytecode, wallet)
   const contract = await factory.deploy()
-  await contract.deployed()
-  console.log(`Deployment successful! Contract Address: ${contract.address}`)
-  return contract;
+  try {
+    await contract.deployed()
+    console.log(`Deployment successful! Contract Address #${i}: ${contract.address}`)
+    return contract
+  } catch(err) {
+    console.log(err)
+  }
 }
 
 async function main() {
 
-  var counters = [];
+  const t0 = performance.now();
 
-  // 100 deployments
-  for(let i=0; i<=99; ++i) {
-    let contract = await deployContract();
-    counters.push(contract);
-    console.log("Deployed contract #"+i);
+  // 90 deployments
+  for(let i=1; i<=90; ++i) {
+    await deployContract(i);
   }
 
-  // Call 10 times 10 different contracts
-  for(let i=0; i<=9; ++i) {
-    let contract = counters[i];
-    for(let j=0; j<9; ++j) {
+  // 10 deployments + push in array
+  for(let i=91; i<=100; ++i) {
+    let contract = await deployContract(i);
+    for(let j=1; j<=10; ++j) {
       await contract.increase();
-      console.log("Called increase() of contract "+contract.address+" for "+j+" times");
+      console.log(`Called increase() of contract ${contract.address} for ${j} times`);
     }
     let finalCount = await contract.getCounter();
-    finalCount++;
-    console.log("This number must be equal to 10: "+finalCount);
+    console.log(`This number must be equal to 10: ${finalCount}`);
   }
+
+  const t1 = performance.now();
+  const elapsedTime = millisToMinutesAndSeconds(t1-t0);
+
+  console.log(`Call to doSomething took ${elapsedTime} milliseconds.`);
 
 }
 
